837f3defd06abc530adcdf43a4e32a3c0078c312
Chaitanya Jain <chaitanyaj.atwork@gmail.com>
2026-08-11 23:12:46 +0530

    Add files via upload

---
+++ Week1/Task1/Program1.java
@@ -0,0 +1,8 @@
+public class Program1{
+    public static void main(String[] args) {
+        System.out.println("Name : Chaitanya jain");
+        System.out.println("College Name: IILM UNIVERSITY");
+        System.out.println("Branch: Computer Science And Engineering");
+        System.out.println("Welcome to my task 1 of internship at internnova");
+    }
+}

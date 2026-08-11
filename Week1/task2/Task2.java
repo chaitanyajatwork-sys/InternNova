3eacce59b40681e9c4785a6e003c1c93196d721b
Chaitanya Jain <chaitanyaj.atwork@gmail.com>
2026-08-11 23:15:20 +0530

    Add files via upload

---
+++ Week1/task2/Task2.java
@@ -0,0 +1,29 @@
+public class Task2 {
+    public static void main(String[] args) {
+        String name;
+        int Age;
+        float Height;
+        float Weight;
+        float CGPA;
+        String gender;
+        Boolean IsStudent;
+
+        name="Chaitanya Jain";
+        Age=18;
+        Height=5.8f;
+        Weight= 58.0f;
+        CGPA=8.48f;
+        gender="Male";
+        IsStudent=true;
+
+        System.out.println("NAME:"+name);
+        System.out.println("AGE:"+Age);
+        System.out.println("HEIGHT:"+Height);
+        System.out.println("WEIGHT:"+Weight);
+        System.out.println("CGPA"+CGPA);
+        System.out.println("GENDER"+gender);
+        System.out.println("Is Student"+IsStudent);
+
+    }
+    
+}

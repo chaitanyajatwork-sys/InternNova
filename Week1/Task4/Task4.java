bf8554c2a5e30b6bc092e691ac815704fe1c92e7
Chaitanya Jain <chaitanyaj.atwork@gmail.com>
2026-08-11 23:17:07 +0530

    Add files via upload

---
+++ Week1/Task4/Task4.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Task4 {
+    public static void main(String[] args) {
+        Scanner in =new Scanner(System.in);
+        int a,b;
+        System.out.println("Enter Two Values");
+        a=in.nextInt();
+        b=in.nextInt();
+
+        System.out.println("Addition");
+        System.out.println(a+b);
+        System.out.println("Subtraction");
+        System.out.println(a-b);
+        System.out.println("Multiplication");
+        System.out.println(a*b);
+        System.out.println("Division");
+        System.out.println(a/b);
+        System.out.println("Modulos");
+        System.out.println(a%b);
+    }
+}
